796eeb7b5ad92aa798bc73e4562b038393085176
Gordon Choi <thefirate@gmail.com>
2026-03-05 16:57:46 +0900

    solve: LeetCode No.1 Two Sum

---
+++ submission/1.swift
@@ -0,0 +1,27 @@
+// LeetCode No.1 Two Sum
+
+class Solution {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        let pairs = nums.enumerated().sorted { $0.element < $1.element }
+
+        for i in 0..<pairs.count - 1 {
+            var left = i + 1
+            var right = pairs.count - 1
+            let searching = target - pairs[i].element
+
+            while left <= right {
+                let mid = (left + right) / 2
+
+                if pairs[mid].element == searching {
+                    return [pairs[i].offset, pairs[mid].offset]
+                } else if pairs[mid].element < searching {
+                    left = mid + 1
+                } else {
+                    right = mid - 1
+                }
+            }
+        }
+        
+        return []
+    }
+}
