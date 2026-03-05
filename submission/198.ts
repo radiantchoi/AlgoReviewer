06f86f8e7347224d6445c6a0afb2c4f3232f8274
Gordon Choi <thefirate@gmail.com>
2026-03-05 14:16:59 +0900

    solve: LeetCode No.198 House Robber

---
+++ submission/198.ts
@@ -0,0 +1,15 @@
+// LeetCode No.198 House Robber
+
+function rob(nums: number[]): number {
+    if (nums.length < 3) {
+        return Math.max(...nums);
+    }
+
+    nums[1] = Math.max(nums[0], nums[1]);
+
+    for (let i = 2; i < nums.length; i++) {
+        nums[i] = Math.max(nums[i - 1], nums[i - 2] + nums[i]);
+    }
+
+    return nums[nums.length - 1];
+};
